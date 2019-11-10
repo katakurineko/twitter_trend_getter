2dad9164caf71bc7663d953659b9c6f4d023329b
ryunosuke_kotaki <ryunosuke.kotaki@mediaseek.co.jp>
2019-11-10 23:42:28 +0900

    ひとまず動くプログラムの作成

--- twitter_trend_getter.py
+++ twitter_trend_getter.py
@@ -8,14 +8,37 @@ Created on Sun Nov 10 21:34:51 2019
 import requests
 from bs4 import BeautifulSoup
 import re
+import datetime
 
-#初回のみ
-target_url = "https://twittrend.jp/time/1118370/2019111018/"
-#Requestsを使って、webから取得
-r = requests.get(target_url)
-#要素を抽出
-soup = BeautifulSoup(r.text, 'lxml')
-#soup.find_allを用いてリンク先が「news.yahoo.co.jp/pickup」の項目を全て取得
-elems = soup.find_all(href=re.compile("twitter.com/search"))
-for e in elems:
-    print(e.getText())
+def createTrendList(datetime):
+    target_url = "https://twittrend.jp/time/1118370/" + datetime.strftime('%Y%m%d%H') + "/"
+    r = requests.get(target_url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    elems = soup.find_all(href=re.compile("twitter.com/search"))
+    list = []
+    for e in elems:
+        list.append(e.text)
+    return list
+
+
+startDate = datetime.datetime(2019,11,4,0)
+endDate = datetime.datetime(2019,11,9,0)
+
+trendLists = []
+
+if(startDate <= endDate):
+    dt = startDate
+    while dt <= endDate:
+        print(dt)
+        trendLists.append( createTrendList(dt))
+        dt += datetime.timedelta(hours=2)
+else:
+    print("end date is before start date")
+
+
+with open("result.csv","w", encoding='utf-8') as f:
+    for i in range(50):
+        for j in range(len(trendLists)):
+            f.write(trendLists[j][i])
+            f.write(",")
+        f.write("\n")
